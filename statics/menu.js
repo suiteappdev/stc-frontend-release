02a7240fb64bad9fab99af81ee9d33dae985f93e
andrew alexander castro vital <armitage@andrews-MacBook-Air.local>
2019-03-31 20:48:24 -0500

    se creo el menu y se mejoro el detalle del producto

---
+++ statics/menu.js
@@ -0,0 +1,82 @@
+window.menu = [	
+	{
+		"menu": "LO NUEVO",
+		"index": 1
+	},
+	{
+		"menu": "JEANS",
+		"index": 2,
+		"submenu": [{
+						"menu": "BOCA CAMPANA",
+						"index": 2.1
+					},
+					{
+						"menu": "BOTA RECTA",
+						"index": 2.2
+					},
+					{
+						"menu": "SKINNY",
+						"index": 2.3
+					},
+					{
+						"menu": "SLIM FIT",
+						"index": 2.4
+					},
+					{
+						"menu": "LEGGINS",
+						"index": 2.5
+					},
+					{
+						"menu": "CULOTTE",
+						"index": 2.6
+					}]
+	},
+	{
+		"menu": "CAMISAS Y BLUSAS",
+		"index": 2
+	},
+	{
+		"menu": "FALDAS Y SHORTS",
+		"index": 3
+	},
+	{
+		"menu": "VESTIDOS Y ENTERIZOS",
+		"index": 4
+	},
+	{
+		"menu": "COMPLEMENTOS",
+		"index": 5,
+		"submenu": [{
+						"menu": "CHAQUETAS",
+						"index": 5.1
+					},
+					{
+						"menu": "BLAZER",
+						"index": 5.2
+				    }]
+	},
+		{
+		"menu": "ACCESORIOS",
+		"index": 6,
+		"submenu": [{
+						"menu": "BOLSOS",
+						"index": 6.1
+					},
+					{
+						"menu": "BISUTERIA",
+						"index": 6.2
+				    },
+									{
+						"menu": "GAFAS",
+						"index": 6.3
+				    }]
+	},
+	{
+		"menu": "SALE",
+		"index": 7
+	},
+	{
+		"menu": "BLOG",
+		"index": 8
+	}
+]
